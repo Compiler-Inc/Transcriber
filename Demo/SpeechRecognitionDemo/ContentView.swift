b46b15289d5bb9bfd077dbc41fdd1f4eb1ba274b
Aurelius Prochazka <aure@aure.com>
2025-02-20 13:09:22 -0800

    Moved default view model into package

--- Demo/SpeechRecognitionDemo/ContentView.swift
+++ Demo/SpeechRecognitionDemo/ContentView.swift
@@ -4,62 +4,6 @@ import SwiftUI
 import SpeechRecognitionService
 import Speech
 
-@Observable
-@MainActor
-public class DefaultSpeechViewModel: SpeechRecognitionManaging {
-    
-    public var isRecording = false
-    public var transcribedText = ""
-    public var authStatus: SFSpeechRecognizerAuthorizationStatus = .notDetermined
-    public var error: Error?
-    
-    public let speechService: SpeechRecognitionService?
-    private var recordingTask: Task<Void, Never>?
-    
-    public init(config: SpeechRecognitionConfiguration = DefaultSpeechConfig()) {
-        self.speechService = SpeechRecognitionService(config: config)
-    }
-    
-    public func requestAuthorization() async throws {
-        guard let speechService else {
-            throw SpeechRecognitionError.noRecognizer
-        }
-        authStatus = await speechService.requestAuthorization()
-        guard authStatus == .authorized else {
-            throw SpeechRecognitionError.notAuthorized
-        }
-    }
-    
-    public func toggleRecording() {
-        guard let speechService else {
-            error = SpeechRecognitionError.noRecognizer
-            return
-        }
-        
-        if isRecording {
-            recordingTask?.cancel()
-            recordingTask = nil
-            isRecording = false
-        } else {
-            recordingTask = Task {
-                do {
-                    isRecording = true
-                    let stream = try await speechService.startRecordingStream()
-                    
-                    for try await transcription in stream {
-                        transcribedText = transcription
-                    }
-                    
-                    isRecording = false
-                } catch {
-                    self.error = error
-                    isRecording = false
-                }
-            }
-        }
-    }
-}
-
 // Example usage in view
 struct ContentView: View {
     @State private var viewModel = DefaultSpeechViewModel()
